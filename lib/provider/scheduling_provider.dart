f042dd4d0282243c248eb4c987e32c6d9cc49641
Andi-IM <andi.irhamm@gmail.com>
2022-04-23 23:31:38 +0700

    update packages

--- lib/provider/scheduling_provider.dart
+++ lib/provider/scheduling_provider.dart
@@ -1,4 +1,4 @@
-import 'package:android_alarm_manager/android_alarm_manager.dart';
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:restaurant/utils/background_service.dart';
 import 'package:restaurant/utils/date_time_provider.dart';
